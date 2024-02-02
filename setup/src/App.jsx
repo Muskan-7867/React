a5f9ebb19895a6577af967c1561ea414aa2cdcc3
muskan <muskanloach984@gmail.com>
2024-02-02 22:29:07 +0530

    class component

--- setup/src/App.jsx
+++ setup/src/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import{ Counters } from './components/Counters';
 import Funcomponents from "./components/Funcomponents";
 import { Counter } from "./components/Counter";
+import Comp from './components/Comp';
 
 export const App = () => {
   return (
     <>
-     <Counter />
+     <Counters />
       <Funcomponents />
+      <Comp />
+      <Counter />
       
     </>
   );
