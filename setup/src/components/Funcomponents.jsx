694fa84a8c20fbfbfd42fdb1375e382d27edebb5
muskan <muskanloach984@gmail.com>
2024-02-02 14:20:41 +0530

    functinal components

---
+++ setup/src/components/Funcomponents.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Funcomponents(){
+    return (
+        <div className="App">
+            <h1>Hello Users</h1>
+        </div>
+    );
+}
+export default Funcomponents;
