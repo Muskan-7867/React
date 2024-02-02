af231422367eefe7ea3bdfcf47f868554961f8bf
muskan <muskanloach984@gmail.com>
2024-02-02 23:09:55 +0530

    nestedif

--- setup/src/components/Nestedif.jsx
+++ setup/src/components/Nestedif.jsx
@@ -6,8 +6,11 @@ function Nestedif() {
 // 1,2,3 users
   return (
     <div>
+        // basically it is a ternary operator
+        
       {
-      Login==1?<h1>Thanks user 1</h1>
+      Login==1?
+      <h1>Thanks user 1</h1>
       :Login==2?<h1>thanks user 2</h1>
       :<h1>Thanks user 3</h1>
       }
