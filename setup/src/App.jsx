11ea4078637e326d6c5140cbd73eed0ea20f4bfc
muskan <muskanloach984@gmail.com>
2024-02-02 22:17:51 +0530

    counters

--- setup/src/App.jsx
+++ setup/src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-
+import{ Counters } from './components/Counters';
 import Funcomponents from "./components/Funcomponents";
+import { Counter } from "./components/Counter";
 
 export const App = () => {
   return (
     <>
-     
+     <Counter />
       <Funcomponents />
       
     </>
