2710b1d2958faba60abc066414e1fb82ef5afb7c
muskan <muskanloach984@gmail.com>
2024-02-03 00:56:42 +0530

    initialize react app with tailwind css

---
+++ react2/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    
+
+      <div className='text-green-500 bg-sky-500 text-8xl	font-bold ' >Hello React</div>
+      <div className=' bg-red-600  hover:bg-cyan-500 ease-in duration-500 shadow-lg rounded-full h-52 w-52 flex justify-center items-center'>
+         <div className='text-white'>circle</div>
+        </div>
+    </>
+  )
+}
+
+export default App
