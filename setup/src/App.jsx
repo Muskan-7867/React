ca140b8cd23f1d034b95c704a54b25f545079ea8
muskan <muskanloach984@gmail.com>
2024-02-02 22:16:56 +0530

    functinal components

--- setup/src/App.jsx
+++ setup/src/App.jsx
@@ -1,56 +1,13 @@
-import { useState } from "react"
-import './App.css'
-import Mul from './Mul'
+import React from "react";
 
-export const App = ()=>{
-// here we write js code
+import Funcomponents from "./components/Funcomponents";
 
-// hook
-const [count , setCount ] = useState(0)
-// usestate return an array 
-
-// first value for show data 
-// second for update
-
-const updatecount = () =>{
-
-  setCount(count+1)
-}
-
-
-
-
-
-
-
-
-
-
-
-
-  return(
-        // return should be one value but we have to
-        // create multiple div which disobey the return 
-        // so we wrape all html code in one div
-  <div>
-    {/* here we write html code  */}
-
-    <div>count: {count} </div>
-    {/* <button onClick={()=>setCount(1)} > Tap  </button> */}
-    <button onClick={updatecount} > Tap  </button>
-    {/* html me js use krne ke liye { }  */}
-
-
-
-
-
-
-
-    <Mul/>
-
-  </div>
-
-  )
-
-}
-
+export const App = () => {
+  return (
+    <>
+     
+      <Funcomponents />
+      
+    </>
+  );
+};
