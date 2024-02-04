cde674e811e015b647157fc6727c2a9f8fc6a5aa
muskan <muskanloach984@gmail.com>
2024-02-04 17:07:36 +0530

    navbar

--- react2/src/component/Navbar.jsx
+++ react2/src/component/Navbar.jsx
@@ -5,15 +5,15 @@ export const Navbar = () => {
     <div className=' bg-black h-12 flex items-center justify-between'>
       <h1 className='text-white font-bold text-2xl ml-1'>TODO APP</h1>
 
-      <ul className="flex ml-4">
-        <li className="mr-2">
+      <ul className="flex ml-4 mr-10">
+        <li className="mr-8">
           <a className='text-white' href='#'>HOME</a>
           {/* a tag nhi use krde agge jake pta lgna a tag refresh kr dinda so react da main km aa no refresh Link tag milna agge pr hje nhi o thoda advance aa. good vdea bnaya design nhi vdea o vdea kr , navbar sohna bna , dhn duja chng krna c bch input vla, ema nu el card vich kr  chl dekh li jida sohna lgge jida phila bnaya c oda hi bna di ok mnu lga muk gya hje te pya a */} 
         </li>
-        <li className="mr-2">
+        <li className="mr-8">
           <a className='text-white' href='#'>ABOUT</a>
         </li>
-        <li className="mr-2">
+        <li className="mr-8">
           <a className='text-white' href='#'>PRICE</a>
         </li>
         <li>
