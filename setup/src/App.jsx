2c43626844db617b71f9b9896addedc52b2a6c2a
muskan <muskanloach984@gmail.com>
2024-02-05 23:36:49 +0530

    props

--- setup/src/App.jsx
+++ setup/src/App.jsx
@@ -1,33 +1,35 @@
-import React ,{useState }from "react";
-import './App.css';
-
 // import{ Counters } from './components/Counters';
 // import Funcomponents from "./components/Funcomponents";
 // import { Counter } from "./components/Counter";
+// import Comp from './components/Comp';
+import  Profile   from './components/Profile'
+import Nestedif   from "./components/nestedif";
+
 // import Comp from './components/Comp';              //class component
 // import  Profile   from './components/Profile';        //ifelse
 // import Nestedif   from "./components/nestedif";       //nestedif
 import Student from "./components/Student";
-import Props from './components/Props';
 export const App = () => {
-  function getData(){
-    alert("Hello!! Today topic is pss function as props")
-  }
-  // const[name,setname] = useState("Muskan")
   return (
+  
     <div className="App">
-
-     
+      <h1>Props in react</h1>
+     {/* <Counters />
+    <div className="App">
+      <h1>Props in react</h1>
       {/* <Counters />
       <Funcomponents />
       <Comp />
+      <Counter /> */}
+      {/* <Profile />
+      <Nestedif />
       <Counter />
        <Profile />
-      <Nestedif /> */}
-      <Props  data={getData}/>
-      
-      
-      
+      <Nestedif /> */} 
+
+    
+      <Student name={"Muskan"} email='muskan@gmail.com'/>
+      <Student name={"Deepak"}  email='deepak@gmail.com'/>
     </div>
   );
 };
