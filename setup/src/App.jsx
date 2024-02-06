5003551dcc118640073457a936877ec831582323
muskan <muskanloach984@gmail.com>
2024-02-06 22:19:28 +0530

    hide, show and toggle

--- setup/src/App.jsx
+++ setup/src/App.jsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { Map } from './components/Map';
 import './App.css';
 
 export const App = () => {
+  const [data , setdata] = useState(false)
   return (
     <div className="App">
-      <h1>Map</h1>
-      <Map />
+      {
+        data ? <h1>Hide and show button</h1>:null
+      }
+      
+      <button onClick={()=> setdata(false)}>Hide</button>
+      <button onClick={()=> setdata(true)}>Show</button> <br /> <br />
+      <div>
+        <button onClick={()=> setdata(!data)}>Toggle</button>
+      </div>
+      {/* same function with toggle */}
     </div>
   );
 };
