2f6efc56418fc7070227bec1c7959e76c0e7995c
muskan <muskanloach984@gmail.com>
2024-02-14 13:29:27 +0530

    react router dom

--- react2/src/App.jsx
+++ react2/src/App.jsx
@@ -1,12 +1,16 @@
-import Weather from "./component/Weather"
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 function App() {
-return (
+  return (
     <>
-      <div>
-        
-     
-      <Weather />
-      </div>
+    <h1>React router dom</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<h1>Home</h1>} />
+          <Route path="/about" element={<h1>about page</h1>} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
