419f88a4586224e706b6bbc7e18472442552378b
muskan <muskanloach984@gmail.com>
2024-02-15 22:13:55 +0530

    Page not found

--- router-app/src/components/Links.jsx
+++ router-app/src/components/Links.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Navbar from './Navbar';
+import Page404 from './Page404';
 
 function Links() {
     return (
@@ -13,6 +14,7 @@ function Links() {
            
             <Route path="/home" element={<Home />} />
             <Route path="/about" element={<About />} />
+            <Route path="/*" element={<Page404 />} />
           </Routes>
         </BrowserRouter>
       
