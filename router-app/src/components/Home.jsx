c97f04f79366943fbb550d679ab990e411b0d83c
muskan <muskanloach984@gmail.com>
2024-02-22 16:06:30 +0530

    navigate

--- router-app/src/components/Home.jsx
+++ router-app/src/components/Home.jsx
@@ -1,11 +1,15 @@
 // About.js
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+  const navigate = useNavigate()
   return (
     <div>
       <h2>HomePage</h2>
       <p>This is the home page content.</p>
+      <br />
+      <button onClick={()=> navigate('about')}>Go to about page</button>
     </div>
   );
 }
