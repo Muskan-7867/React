ffb9333486afcee18c222e81b0d95f5559672b54
muskan <muskanloach984@gmail.com>
2024-03-08 14:42:11 +0530

    complete albumdetails

--- Jio savaan/jio savaan/src/Components/Albumitem.jsx
+++ Jio savaan/jio savaan/src/Components/Albumitem.jsx
@@ -2,12 +2,27 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Albumitem = ({ name, artists, id, image }) => {
+  const artistNames = artists.map((artist) => artist.name).join(", ");
+  const shortenedArtistNames =
+    artistNames.length > 24 ? artistNames.slice(0, 24) + "...." : artistNames;
+
   return (
-    <Link to={`/albums/${id}`} className=" w-[160px] max-h-[220px] overflow-y-clip flex  flex-col justify-center items-center gap-3 rounded-lg">
-      <img src={image[2].link} alt={name} style={{ width: "100%", height: "auto" }} />
+    <Link
+      to={`/albums/${id}`}
+      className="w-[160px] max-h-[220px] overflow-y-clip flex flex-col justify-center items-center gap-3 rounded-lg"
+    >
+      <img
+        src={image[2].link}
+        alt={name}
+        style={{ width: "100%", height: "auto" }}
+      />
       <div className="text-[13px] w-full flex flex-col justify-center items-center">
-        <span className="text-gray-600 font-semibold overflow-x-clip" style={{ fontSize: "14px" }}>{name}</span>
-        <p className="text-gray-500 font-thin" style={{ fontSize: "12px" }}>{artists.map((artist) => artist.name).join(", ")}</p>
+        <span className="text-gray-600 font-semibold overflow-x-clip">
+          {name}
+        </span>
+        <p className="text-gray-500 font-thin">
+          {shortenedArtistNames}
+        </p>
       </div>
     </Link>
   );
