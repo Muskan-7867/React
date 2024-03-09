d53d0b940b55c612513f6012a20611a1c3df7d9e
muskan <muskanloach984@gmail.com>
2024-03-10 00:38:35 +0530

    trigerred scroll

--- Framer-motion/src/Components/Triggeredscroll.jsx
+++ Framer-motion/src/Components/Triggeredscroll.jsx
@@ -1,26 +1,33 @@
-import React from 'react'
-import {motion} from 'framer-motion'
+import React from 'react';
+import { motion } from 'framer-motion';
 
 export const Triggeredscroll = () => {
   return (
     <div className='flex flex-col items-center'>
-        <h2 className='text-2xl text-black'>React Scroll triggered Animation using framer-motion</h2>
-        <Box />
-        <Box />
-        <Box />
+      <h2 className='text-2xl text-black'>React Scroll triggered Animation using framer-motion</h2>
+      <Box />
+      <Box />
+      <Box />
+      <Box />
+      <Box />
+      <Box />
+      <Box />
+      <Box /> 
+      <Box />
     </div>
-  )
+  );
+};
+
+function Box() {
+  return (
+    <motion.div
+      className='w-32 h-32 bg-blue-800 m-32 mt-5'
+      initial={{ opacity: 0, scale: 0 }}
+      whileInView={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.6 }}
+    >
+    </motion.div>
+  );
 }
 
-function Box(){
-    return(
-        <motion.div className='w-[100px] h-[100px] bg-blue-800 m-32 mt-5'
-        initial={{opacity:0,scale:0}}
-        whileInView={{opacity:1,scale :1}}
-        transition={{duration:0.6}}>
-          
-        </motion.div>
-    )
-}
-
-export default Triggeredscroll
+export default Triggeredscroll;
