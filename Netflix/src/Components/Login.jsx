f200a23315ca1e0a202bd958dc5c510573662128
muskan <muskanloach984@gmail.com>
2024-04-08 22:10:47 +0530

    Sign in/Up form

--- Netflix/src/Components/Login.jsx
+++ Netflix/src/Components/Login.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 
 export const Login = () => {
+  const[isSigninform, setisSigninform] = useState(true);
+
+  const togglesignform = () => {
+setisSigninform(!isSigninform);
+  }
   return (
     <div>
       <Header />
@@ -9,11 +14,17 @@ export const Login = () => {
         <img className='h-screen w-full object-cover ' src='src/assets/netflix.jpg' alt="Netflix" />
       </div>
 
-      <form className='absolute bg-black right-0 left-0 top-1/2 transform -translate-y-1/2 mx-auto p-12 w-3/12'>
-        <h1 className='text-white text-3xl font-bold mb-6'>Sign-In</h1>
-        <input type='text' placeholder='Email-Address' className='py-4 px-4 mb-4 w-full bg-gray-800 text-white border border-gray-700 rounded'></input>
-        <input type='password' placeholder='Password' className='py-4 px-4 mb-4 w-full bg-gray-800 text-white border border-gray-700 rounded'></input>
-        <button className='py-2 px-4 bg-red-700 text-white w-full rounded hover:bg-red-600'>Sign in</button>
+      <form className='absolute right-0 left-0 top-1/2 bg-black bg-opacity-80  rounded-lg transform -translate-y-1/2 mx-auto p-12 w-3/12'>
+        <h1 className='text-white text-3xl font-bold mb-6'>
+          { isSigninform ? "Sign-In" : "Sign-Up" }
+        </h1>
+        <input type='text' placeholder='Email-Address' className='py-4 px-4 mt-4 mb-4 w-full bg-gray-800 text-white border border-gray-700 rounded'></input>
+        <input type='password' placeholder='Password' className='py-4 px-4 mt-6 mb-4 w-full bg-gray-800 text-white border border-gray-700 rounded'></input>
+        <button className='py-2 px-4 bg-red-700 mt-6 text-white w-full rounded hover:bg-red-600'>
+        { isSigninform ? "Sign-In" : "Sign-Up" }
+        </button>
+        <p onClick={togglesignform} className='text-white mt-5'>
+          {isSigninform ? "New to Netflix? Sign Up Now" : "Already account? Sign in Now" }</p> 
       </form>
     </div>
   );
