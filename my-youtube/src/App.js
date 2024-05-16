b01ef6f4e4b983203d2f4068f63b86fa2120632d
muskan <muskanloach984@gmail.com>
2024-05-16 14:44:26 +0530

    wrap store into component

--- my-youtube/src/App.js
+++ my-youtube/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import Body from './components/Body'
 import Head from './components/Head'
+import { Provider } from 'react-redux'
+import store from './store/store'
 
 function App() {
   return (
+    <Provider store={store}>
     <div>
      <Head />
      <Body />
     </div>
+    </Provider>
   )
 }
 
