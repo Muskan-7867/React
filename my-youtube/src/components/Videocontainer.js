175bb86b54049edc1c5b483d50c7927ff1f356bd
muskan <muskanloach984@gmail.com>
2024-05-30 23:09:08 +0530

    videocontainer

---
+++ my-youtube/src/components/Videocontainer.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import { youtube_video_api } from '../utils/constants';
+import Videocard from './Videocard';
+
+const Videocontainer = () => {
+  const [videos, setVideos] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getVideos();
+  }, []);
+
+  const getVideos = async () => {
+    try {
+      const response = await fetch(youtube_video_api);
+      const json = await response.json();
+      setVideos(json.items);
+    } catch (error) {
+      console.error('Error fetching videos:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+
+
+  return (
+    <div className='flex'>
+      
+        {videos.map(video=> <Videocard key={videos.id} info={video}  />)}
+      
+    </div>
+  );
+};
+
+export default Videocontainer;
