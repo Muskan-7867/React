90b23aea49dfc511470e523d28e46766cbc83aec
muskan <muskanloach984@gmail.com>
2024-06-01 23:25:52 +0530

    wrap our app with store

--- Netflix/src/App.jsx
+++ Netflix/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Body from './Components/Body';
+import { Provider } from 'react-redux';
+import appstore from '../utills/appstore';
 
 export const App = () => {
   return (
-    <div>
-      <Body />
-    </div>
+    
+    <Provider store={appstore} >
+         <Body />
+    </Provider>
   )
 }
 export default App;
