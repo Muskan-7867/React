6dbdacc976900327eb6c860d2fe3fced66f0a822
muskan <muskanloach984@gmail.com>
2024-06-04 15:35:45 +0530

    dispatch an action on watchpg

--- my-youtube/src/components/Watchpage.js
+++ my-youtube/src/components/Watchpage.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { closeMenu } from '../store/reducers/appSlice'
 
 const Watchpage = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(closeMenu())
+  })
   return (
     <div>Watchpage</div>
   )
