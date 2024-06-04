f66505ebce0a9f0813ae51637bd0b963c82ff3ad
muskan <muskanloach984@gmail.com>
2024-06-04 22:20:24 +0530

    change onauthstate

--- Netflix/src/Components/Body.jsx
+++ Netflix/src/Components/Body.jsx
@@ -1,16 +1,8 @@
-import React, { useEffect  } from 'react'
-import {  createBrowserRouter, RouterProvider, useNavigate } from 'react-router-dom';
+
+import {  createBrowserRouter, RouterProvider  } from 'react-router-dom';
 import Login from './Login';
 import Browse from './Browse';
-
-import { onAuthStateChanged } from "firebase/auth";
-import { useDispatch } from 'react-redux';
-import { addUser, removeuser } from '../../utills/userSlice';
-import { auth } from '../../utills/firebaseConfig';
-
-
 export const Body = () => {
-  const dispatch = useDispatch();
  
  const appRouter = createBrowserRouter([
 {
@@ -22,26 +14,7 @@ export const Body = () => {
   element:<Browse />
 }
 ]);
-
-useEffect(() => {
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-    
-      const {uid,email,photoURL,displayName} = user;
-      dispatch(addUser({uid, email, photoURL:photoURL,displayName}))
-      
-      // ...
-    } else {
-      // User is signed out
-      dispatch(removeuser());
-      
-    }
-  });
-  
-})
-
-
-  return (
+return (
   <div>
  <RouterProvider router={appRouter} />
 
