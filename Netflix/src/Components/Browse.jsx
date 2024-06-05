5524dc8000437359f1a309d0141f5f5fd748a7c6
muskan <muskanloach984@gmail.com>
2024-06-05 22:08:25 +0530

    get data from tmdb now playing movies list api

--- Netflix/src/Components/Browse.jsx
+++ Netflix/src/Components/Browse.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from './Header';
+import { json } from 'react-router-dom';
+import { API_options } from '../../utills/constants';
 
 export const Browse = () => {
+  const getNowPlayingMovies = async  () => {
+    const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_options)
+    const json =  await data.json();
+    console.log(json)
+  }
+
+  useEffect(() => {
+    getNowPlayingMovies();
+  },[])
   return (
     <div>
         <Header />
