23f384844bf93b65bff2d771fb383b7a7d3c0375
muskan <muskanloach984@gmail.com>
2024-06-05 22:09:05 +0530

    get data from tmdb

--- Netflix/utills/constants.js
+++ Netflix/utills/constants.js
@@ -1,2 +1,10 @@
 // constants.js
 export const LOGO = 'https://exchange4media.gumlet.io/news-photo/130760-net.webp?w=400&dpr=2.6' // Ensure this path is correct
+
+export const API_options  = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZTg5OWFhOTA5MmVhZDYyYzY1YzhjZTM3ZGYxOGU5NyIsInN1YiI6IjY2NjAxOWI1MDM1ODBjOTY0MTRmNWU5NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sGudFa8DJ3jeTLQ_80Fm1_JwOa7Xev8qkH6hyvsb08M'
+        }
+      };
