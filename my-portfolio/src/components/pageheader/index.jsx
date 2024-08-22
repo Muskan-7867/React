1b82669f03e71ff2a0c3ff030967c0a37e0536c3
muskan <muskanloach984@gmail.com>
2024-08-22 23:15:57 +0530

    pageheader

--- my-portfolio/src/components/pageheader/index.jsx
+++ my-portfolio/src/components/pageheader/index.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import "./style.css";
+
 
 const Pageheader = (props) => {
   const { headertext, icon } = props;
   return (
-    <section class="text-center mt-16 flex justify-between border-b border-b-orange-[1px] ">
-      <h1 class="text-4xl font-bold mb-4 text-orange-500">{headertext}</h1>
-      <span className="p-2 rounded-full text-orange-500">{icon}</span>
+    <section class="text-center mt-16 flex justify-between border-b border-[#A52A2A]  ">
+      <h1 class="text-4xl font-bold mb-4 text-[#A52A2A]">{headertext}</h1>
+      <span className="p-2 rounded-full text-[#A52A2A]">{icon}</span>
     </section>
   );
 };
