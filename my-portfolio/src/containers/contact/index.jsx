8c831d02cc7ef97e23f1ace49a41e76b0036c419
muskan <muskanloach984@gmail.com>
2024-08-22 23:19:15 +0530

    contact page

--- my-portfolio/src/containers/contact/index.jsx
+++ my-portfolio/src/containers/contact/index.jsx
@@ -1,13 +1,39 @@
-import React from 'react'
-import { BsInfoCircleFill } from 'react-icons/bs'
-import Pageheader from '../../components/pageheader'
+import React from 'react';
+import { BsPersonFill, BsEnvelopeFill, BsFillFileTextFill } from 'react-icons/bs';
+import Pageheader from '../../components/pageheader';
 
 const Contact = () => {
   return (
-    <section className="p-8">
-    <Pageheader headertext="Contact Me" icon={<BsInfoCircleFill size={30} />} />
-  </section>
-  )
+    <section className="p-8 border border-[#A52A2A] ">
+      <Pageheader headertext="Contact Me" icon={<BsPersonFill size={30} />} />
+      <div className="mt-6 border-t border-[#A52A2A]  pt-4">
+        <div className="mb-12 border border-[#A52A2A] p-4 flex items-center animate-slide-in-left ">
+          <BsPersonFill size={24} className="mr-3 text-[#A52A2A]" />
+          <div>
+            <h2 className="text-xl font-semibold mb-2 text-[#A52A2A] slide-In-Left">Name</h2>
+            <p className='text-[#A52A2A]'>Muskan</p>
+          </div>
+        </div>
+        <div className="mb-12 border border-[#A52A2A]  p-4 flex items-center animate-slideInRight">
+          <BsEnvelopeFill size={24} className="mr-3 text-[#A52A2A]" />
+          <div>
+            <h2 className="text-xl font-semibold mb-2 text-[#A52A2A]">Email</h2>
+            <p className='text-[#A52A2A]'>muskanloach984@gmail.com</p>
+          </div>
+        </div>
+        <div className="border border-[#A52A2A]  p-4 flex items-center animate-slide-in-left ">
+          <BsFillFileTextFill size={24} className="mr-3 text-[#A52A2A]" />
+          <div>
+            <h2 className="text-xl font-semibold mb-2 text-[#A52A2A]">Description</h2>
+            <p className='text-[#A52A2A]'>
+              I am a passionate developer with experience in building web applications.
+              Feel free to reach out if you have any questions or opportunities!
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Contact
+export default Contact;
