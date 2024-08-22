b7c2983d3387358c52996d3565270ac8004c02d9
muskan <muskanloach984@gmail.com>
2024-08-22 23:17:34 +0530

    skills page

--- my-portfolio/src/containers/skills/index.jsx
+++ my-portfolio/src/containers/skills/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BsInfoCircleFill } from 'react-icons/bs';
+import { FaStar } from 'react-icons/fa';
 import Pageheader from '../../components/pageheader';
 import { skillsdata } from './utills';
 import { Line } from 'rc-progress';
@@ -7,8 +7,8 @@ import { Line } from 'rc-progress';
 const Skills = () => {
   return (
     <>
-      <section className="p-8 text-yellow-400">
-        <Pageheader headertext="My Skills" icon={<BsInfoCircleFill size={30} />} />
+      <section className="p-8 text-[#A52A2A]">
+        <Pageheader headertext="My Skills" icon={<FaStar size={30} />} />
       </section>
 
       {/* Skill Categories with Labels and Skills */}
@@ -16,7 +16,7 @@ const Skills = () => {
         {skillsdata.map((item, i) => (
           <div key={i} className="p-4">
             {/* Skill Category Label */}
-            <h3 className="text-2xl font-bold text-start text-orange-500 transition-transform duration-500 ease-in-out animate-slide-in-left mb-4">
+            <h3 className="text-2xl font-bold text-start text-[#A52A2A] transition-transform duration-500 ease-in-out animate-slide-in-left mb-4">
               {item.label}
             </h3>
 
@@ -27,13 +27,13 @@ const Skills = () => {
                   key={j}
                   className="transition-transform duration-500 ease-in-out animate-slide-in-left"
                 >
-                  <p className="text-sm font-semibold mb-2 text-white">
+                  <p className="text-sm font-semibold mb-2 text-[#A52A2A]">
                     {skill.skillName}
                   </p>
                   <Line 
                     percent={skill.percentage}
                     strokeWidth="2"
-                    strokeColor="orange"
+                    strokeColor="#A52A2A"
                     trailWidth="2"
                     trailColor="white"
                     strokeLinecap="square"
